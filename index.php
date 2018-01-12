d600ffefcb7e98893310d1ee7b81102c2a1fa103
Mels Ramblings <mfelton@mail.greenriver.edu>
2018-01-12 16:53:02 -0500

    Corrected typo in index.php. Added Bootstrap and subheader to view/home.html.

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 
     //Define a default route
     $f3->route('GET /', function() {
-        //echo "<h1>Pet Home</h1>";
+//        echo "<h1>Pet Home</h1>";
         $view = new View();
-        echo $view ->rendor("views/home.html");
+        echo $view ->render("views/home.html");
     });
 
     //Run Fat-Free
